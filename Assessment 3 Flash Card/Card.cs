826cb5c3bc9a8b09fe80f4d17b9fb14e6328b0af
Cattress <priyaphon.vincent@gmail.com>
2021-03-15 12:14:49 +0700

    Add project files.

---
+++ Assessment 3 Flash Card/Card.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_3_Flash_Card
+{
+    class Card
+    {
+        private bool flipped;
+        private string cardTitle;
+        private string cardDefinition;
+        private int topCard;
+
+        public Card()
+        {
+        }
+
+        public string GetDefinition()
+        {
+            return cardDefinition;
+        }
+
+        public string
+    }
+}
